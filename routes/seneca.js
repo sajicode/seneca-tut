3dfbed9a5a28420a776fea37083b6ee587d32a69
sajicode <sajioloye@gmail.com>
2019-08-10 22:29:55 +0100

    chain seneca results

--- routes/seneca.js
+++ routes/seneca.js
@@ -2,14 +2,20 @@ const router = require('express').Router();
 const seneca = require('seneca')();
 
 router.get('/math', (req, res) => {
+	//* add two numbers
 	seneca.add('role:math,cmd:sum', (msg, reply) => {
 		reply(null, { answer: msg.left + msg.right });
 	});
 
-	seneca.act({ role: 'math', cmd: 'sum', left: 1, right: 2 }, (err, result) => {
-		if (err) console.error(err);
-		console.log(JSON.stringify(result, null, 2));
+	//* multiply two numbers
+	seneca.add('role: math, cmd: product', (msg, respond) => {
+		const product = msg.left * msg.right;
+		respond(null, { reply: product });
 	});
+
+	seneca
+		.act({ role: 'math', cmd: 'sum', left: 1, right: 2 }, console.log)
+		.act({ role: 'math', cmd: 'product', left: 4, right: 5 }, console.log);
 
 	res.send('Seneca in Action');
 });
