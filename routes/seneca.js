3b5d9c56bcaf2caca4a08fb197ab0136b9de619c
sajicode <sajioloye@gmail.com>
2019-08-11 23:46:56 +0100

    senceca actions with specificity

--- routes/seneca.js
+++ routes/seneca.js
@@ -31,4 +31,34 @@ router.get('/maths', (req, res) => {
 	res.send('Extending Seneca functionality');
 });
 
+//? adding more patterns to a system
+//* seneca chooses the one to use by specificity i.e.
+//* the pattern with the highest number of attributes has precedence
+
+router.get('/specific', (req, res) => {
+	seneca.add({ role: 'math', cmd: 'sum' }, (msg, respond) => {
+		var sum = msg.left + msg.right;
+		respond(null, { answer1: sum });
+	});
+
+	//* the next two messages match role: math, cmd: sum
+	seneca
+		.act({ role: 'math', cmd: 'sum', left: 1.5, right: 2.5 }, console.log)
+		.act({ role: 'math', cmd: 'sum', left: 1.5, right: 2.5 }, console.log);
+
+	seneca.add({ role: 'math', cmd: 'sum', integer: true }, (msg, respond) => {
+		var sum = Math.floor(msg.left) + Math.floor(msg.right);
+		respond(null, { answer2: sum });
+	});
+
+	//* the next message still matches role: math, cmd: sum
+	seneca.act({ role: 'math', cmd: 'sum', left: 1.5, right: 2.5 }, console.log);
+
+	//* the next msg matches role:math,cmd:sum,integer:true
+	//* because it's more specific - more properties match
+	seneca.act({ role: 'math', cmd: 'sum', left: 1.5, right: 2.5, integer: true }, console.log);
+
+	res.send('Seneca Specificity');
+});
+
 module.exports = router;
