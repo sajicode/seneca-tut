23db949dad105b13759487158c71cb246c96bf1f
sajicode <sajioloye@gmail.com>
2019-08-13 20:44:11 +0100

    enhancing patterns and debugging methods

--- routes/seneca.js
+++ routes/seneca.js
@@ -89,4 +89,45 @@ router.get('/reuse', (req, res) => {
 	res.send('Code Reuse with patterns');
 });
 
+//* Defined action patterns are unique & can only trigger one function
+//* Patterns resolve using the following rules..
+//* 1. More properties win
+//* 2. If the patterns have the same number of properties, they are matched in alphabetical
+
+//* Enhancement & Validation checks w/ debugging info
+router.get('/enhance', (req, res) => {
+	seneca
+		.add('role:math,cmd:sum', (msg, respond) => {
+			let sum = msg.left + msg.right;
+			respond(null, { answer: sum });
+		})
+		//* override role:math,cmd:sum with additional functionality
+		.add('role:math,cmd:sum', function(msg, respond) {
+			//* bail out early if there is a problem
+			if (!Number.isFinite(msg.left) || !Number.isFinite(msg.right)) {
+				return respond(new Error('Expected left and right to be numbers'));
+			}
+
+			//* call previous action function for role:math,cmd:sum
+			this.prior(
+				{
+					role: 'math',
+					cmd: 'sum',
+					left: msg.left,
+					right: msg.right
+				},
+				(err, result) => {
+					if (err) return respond(err);
+
+					result.info = msg.left + '+' + msg.right;
+					respond(null, result);
+				}
+			);
+		})
+		//* enhanced role:math,cmd:sum
+		.act('role:math,cmd:sum,left:1.5,right:2.5', console.log);
+	//* prints {answer: 4, info: '1.5+2.5'}
+	res.send('Enhanced patterns');
+});
+
 module.exports = router;
